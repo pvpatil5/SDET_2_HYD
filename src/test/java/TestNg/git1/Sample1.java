1d5300aaaef989afb5d778cd6d39537d52a6311b
pvpatil5 <pvpatil123@yahoo.com>
2021-11-15 11:09:46 +0530

    hi

---
+++ src/test/java/TestNg/git1/Sample1.java
@@ -0,0 +1,12 @@
+package TestNg.git1;
+
+import org.testng.annotations.Test;
+
+public class Sample1 {
+
+	@Test
+	public void s1() {
+		System.out.println("s1");
+	}
+	
+}
--- src/test/java/TestNg/practice/AkshayTest.java
+++ src/test/java/TestNg/practice/AkshayTest.java
@@ -4,7 +4,6 @@ public class AkshayTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("AkshayTest");
+		System.out.println("AkshayTest");
 	}
-
 }
